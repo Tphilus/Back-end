2137ce514c064807f8f0f5d2b66a7d159464817b
Tphilus <tkdadzie@st.ug.edu.gh>
2024-03-26 12:25:10 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = ` #graphql
+    type Query {
+        greeting: String
+    }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello world",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, { listen: { port: 9000 } });
+console.log(`Server running at ${url}`);
