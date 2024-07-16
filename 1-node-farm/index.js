f6176bed6998af16c47e42093979ca1d858786a1
Tphilus <tkdadzie@st.ug.edu.gh>
2024-07-16 10:48:55 -0700

    update on node and how node works

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
@@ -11,35 +11,32 @@ const url = require("url");
 const data = fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-
-
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-    
+  const pathName = req.url;
 
-    if(pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW')
-    } else if (pathName === '/product') {
-        res.end('This is the PRODUCT')
-    } else if(pathName === '/api') { 
-        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-           const productData = JSON.parse(data);
-           res.writeHead(200, {
-            'Content-type': 'application/json'
-           } )
-           res.end(data)
-        })
-    } else {
-        res.writeHead(404, {
-            'Content-type': "text/html",
-            'my-own-header': 'hello-world'
-        })
-        res.end("<h1>Page not found!</h1>")
-    } 
+  // Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+
+    // Product page
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+
+    // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log("List to requests on port 8000");
-})
-
-
+server.listen(8000, "127.0.0.1", () => {
+  console.log("List to requests on port 8000");
+});
