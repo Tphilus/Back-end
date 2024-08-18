5db75db639784620a52869a53329db7f726e49b2
Tphilus <tkdadzie@st.ug.edu.gh>
2024-08-18 12:16:24 -0700

    feat: add get, git by id and post request for the tours

--- natours/app.js
+++ natours/app.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // Midleware
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Hello from the server side', app: "Natours"});
@@ -13,22 +14,68 @@ const app = express();
 // })
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    // if(id > tours.length) {
+    if(!tour) {
+        return res.status(404).json({
+            status: "fail",
+            message: 'Invalid ID'
+        })
+    }
+
+    
     res.status(200).json({
         status: 'success',
-        results: tours.length,
         data: {
-            tours
+            tour
         }
     })
 })
 
-app.post('/app/vi/tours')
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        },
+      });
+    }
+  );
+
+//   res.send('Done');
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App runing on port ${port}...`);
-})
+  console.log(`App runing on port ${port}...`);
+});
