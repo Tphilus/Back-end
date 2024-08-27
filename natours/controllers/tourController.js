f23cbad7da87299fa222bcad6c3bb998db957ded
Tphilus <tkdadzie@st.ug.edu.gh>
2024-08-26 17:25:44 -0700

    feat: update the tourRouter and tourControllers

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -5,6 +5,17 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
   );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+}
+
   exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
     res.status(200).json({
@@ -23,13 +34,13 @@ const tours = JSON.parse(
     const id = req.params.id * 1;
     const tour = tours.find((el) => el.id === id);
   
-    // if(id > tours.length) {
-    if (!tour) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid ID',
-      });
-    }
+    // // if(id > tours.length) {
+    // if (!tour) {
+    //   return res.status(404).json({
+    //     status: 'fail',
+    //     message: 'Invalid ID',
+    //   });
+    // }
   
     res.status(200).json({
       status: 'success',
@@ -63,12 +74,12 @@ const tours = JSON.parse(
   };
   
   exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'Invalid ID',
-      });
-    }
+    // if (req.params.id * 1 > tours.length) {
+    //   return res.status(404).json({
+    //     status: 'fail',
+    //     message: 'Invalid ID',
+    //   });
+    // }
   
     res.status(200).json({
       status: 'success',
--- natours/routers/tourRouters.js
+++ natours/routers/tourRouters.js
@@ -4,10 +4,12 @@ const tourController = require('./../controllers/tourController')
 
 const router = express.Router();
 
-router.param('id', (req, res, next, val) => {
-    console.log(`Tour id is ${val}`);
-    next();
-})
+// router.param('id', (req, res, next, val) => {
+//     // console.log(`Tour id is ${val}`);
+//     next();
+// })
+
+router.param('id', tourController.checkID)
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
