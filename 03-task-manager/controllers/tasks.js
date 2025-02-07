d6299688e5fa9f430ee05564d3ab1febec187802
Tphilus <tkdadzie@st.ug.edu.gh>
2025-02-07 11:55:30 +0000

    add: can add, get all tasks, get a single task and delete a task

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -1,26 +1,54 @@
-const Task = require('../models/Task')
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const Task = require("../models/Task");
+const getAllTasks = async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(200).json({ tasks: task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTasks = async (req, res) => {
-  const task = await Task.create(req.params)
-  res.status(201).json({
-    message: "Task created",
-    task
-  })
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({
+      task,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({id:req.param.id})
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ deleteTask: task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).jsonw;
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
