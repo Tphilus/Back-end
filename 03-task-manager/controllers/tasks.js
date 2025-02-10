75087c1fccbdbb638258d70e6dac9ede3bf6a06c
Tphilus <tkdadzie@st.ug.edu.gh>
2025-02-10 21:34:56 +0000

    update

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -27,25 +27,45 @@ const getTask = async (req, res) => {
     if (!task) {
       return res.status(404).json({ msg: `No task with id : ${taskID}` });
     }
+    res.status(200).json({ tasks: task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    // ID for the params
+    const { id: taskID } = req.params;
+
+    // finding the same unique ID to delete it
+    const task = await Task.findByIdAndDelete({ _id: taskID });
+
+    // if the Unique ID is not the same
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
     res.status(200).json({ deleteTask: task });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update task");
-};
-
-const deleteTask = async (req, res) => {
+const updateTask = async (req, res) => {
   try {
+    // ID for the params
     const { id: taskID } = req.params;
-    const task = await Task.findByIdAndDelete({ _id: taskID });
 
+    // finding the same unique ID to update it
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    // if the Unique ID is not the same
     if (!task) {
       return res.status(404).json({ msg: `No task with id : ${taskID}` });
     }
-    res.status(200).jsonw;
   } catch (error) {
     res.status(500).json({ msg: error });
   }
