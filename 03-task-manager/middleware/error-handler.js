0c39c39f19f746cd44ca86890e8e9b2ecf00896c
Tphilus <tkdadzie@st.ug.edu.gh>
2025-02-15 19:50:13 +0000

    add custom error to the page

--- 03-task-manager/middleware/error-handler.js
+++ 03-task-manager/middleware/error-handler.js
@@ -1,5 +1,7 @@
 const errorHandleMiddleware = (err, req, res, next) => {
-  return res.status(500).json({ msg: "something went wrong, try again later" });
+  console.log(err);
+  // return res.status(500).json({ msg: "something went wrong, try again later" });
+  return res.status(err.status).json({ msg: err.message });
 };
 
 module.exports = errorHandleMiddleware;
