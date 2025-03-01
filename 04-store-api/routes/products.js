e2fac5977df11f94a77054909f75b2830b9e3cd9
Tphilus <tkdadzie@st.ug.edu.gh>
2025-03-01 21:42:02 +0000

    update

--- 04-store-api/routes/products.js
+++ 04-store-api/routes/products.js
@@ -1,2 +1,11 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
+
+const {
+  getAllProducts,
+  getAllProductsStatic,
+} = require("../controllers/products");
+
+router.route("/").get(getAllProducts);
+router.route("/static").get(getAllProductsStatic);
+
