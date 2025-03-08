11fa2fd70c463f20e37e9f4f7a1eb513bf508940
Tphilus <tkdadzie@st.ug.edu.gh>
2025-03-08 06:51:27 +0000

    bacdend setup

--- mern-tutorial/backend/server.js
+++ mern-tutorial/backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello, world");
+require("dotenv").config();
+
+const express = require("express");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, (req, res) => {
+  console.log(`Server start on port ${port}`);
+});
