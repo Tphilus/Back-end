164a8a7165b0271ce8a7dca7ffee597ed7726929
Tphilus <tkdadzie@st.ug.edu.gh>
2025-03-09 14:01:48 +0000

    add the router

--- mern-tutorial/backend/server.js
+++ mern-tutorial/backend/server.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.get("api/goals", (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+});
+
 app.listen(port, (req, res) => {
   console.log(`Server start on port ${port}`);
 });
