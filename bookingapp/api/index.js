7cf6737c3fa2ca9a689968dc71a3856aeda330c2
Tphilus <tkanateydadzie@gmail.com>
2025-05-12 20:34:36 -0700

    initial backend project, add router and mongoDB connect

---
+++ bookingapp/api/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
+
+const app = express();
+dotenv.config();
+
+// Mongo Connect
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+// middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+
+app.listen(8000, () => {
+  connect();
+  console.log("Connected to backend!");
+});
