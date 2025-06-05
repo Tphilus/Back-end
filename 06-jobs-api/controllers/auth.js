dd73fb778236bd83369e32aefa5e0c4af19492f4
Tphilus <tkanateydadzie@gmail.com>
2025-06-05 00:59:38 -0700

    add: register contollers

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,5 +1,23 @@
+const { BadRequestError } = require("../errors");
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-  res.send("register user");
+  // ===== ===  hashing password
+  //   const { name, email, password } = req.body;
+
+  //   const salt = await bcrypt.genSalt(10);
+  //   const hashedPassword = await bcrypt.hash(password, salt);
+
+  //   const tempUser = { name, email, password: hashedPassword };
+  // ===== === End of  hashing password
+
+  //   if (!name || !email || !password) {
+  //     throw new BadRequestError("Please provide name, email and password");
+  //   }
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
---
+++ 06-jobs-api/models/User.js
@@ -0,0 +1,33 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 6,
+  },
+});
+
+// Password hashing
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+module.exports = mongoose.model("User", UserSchema);
