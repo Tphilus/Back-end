415356cbdc31e9685bdb511b54111b5229bdfeb1
Tphilus <tkanateydadzie@gmail.com>
2025-06-19 00:33:10 -0700

    update: 06.5 project and add new 07-file-project

--- 06.5-jobster-api/controllers/jobs.js
+++ 06.5-jobster-api/controllers/jobs.js
@@ -1,6 +1,8 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
+const mongoose = require("mongoose");
+const moment = require("moment");
 
 const getAllJobs = async (req, res) => {
   // to query all the function
@@ -118,9 +120,55 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  res
-    .status(StatusCodes.OK)
-    .json({ defaultStats: {}, monthlyApplications: [] });
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = await Job.aggregate([
+
+
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 8 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y');
+      return { date, count };
+    })
+    .reverse();
+
+  // console.log(monthlyApplications);
+  
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 module.exports = {
--- 07-file-upload/middleware/authentication.js
+++ 07-file-upload/middleware/authentication.js
@@ -13,8 +13,7 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    const textUser = (payload.userId === "6852a89c40995cfbdc3192e4")
-    req.user = { userId: payload.userId, textUser }
+    req.user = { userId: payload.userId, name: payload.name }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
