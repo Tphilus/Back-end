342a072b42bca9a0d1e72e4b13e00bf69c97601b
Tphilus <tkanateydadzie@gmail.com>
2025-07-09 17:27:21 -0700

    modified the files

--- typescript-rest-api/src/app.ts
+++ typescript-rest-api/src/app.ts
@@ -1,18 +1,16 @@
 import express from 'express';
+import cors from "cors";
 import config from './config/config';
 import ConnectDB from './utils/connect';
 import logger from './utils/logger';
-import routes from './routes/routes';
 
 const port = config.port;
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 
 app.listen(port, async () => {
 //   console.log(`Server is listening on port ${port}...`);
   logger.info(`Server is listening on http://localhost:${port}`)
   await ConnectDB();
-
-  routes(app)
 });
