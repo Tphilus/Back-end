67f5153b4283f504e9df25cd21bfa3e9473c8a54
Tphilus <tkanateydadzie@gmail.com>
2025-10-16 21:56:42 +0000

    add: gitignore

--- fastapi/fastapi_learn1/main.py
+++ fastapi/fastapi_learn1/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -11,7 +18,9 @@ def root():
 def get_posts():
      return {"data": "This is your post"}
  
-@app.post("/creatpost")
-def creat_posts(payload: dict = Body(...)):
-    return {"message":"Successfully created a posts"}    
-      
+@app.post("/createposts")
+# def creat_posts(payload: dict = Body(...)):
+def creat_posts(new_post: Post):
+    print(new_post)
+    return {"data": "new post"}    
+
