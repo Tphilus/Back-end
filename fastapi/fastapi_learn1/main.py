e8c87ac241cbacdee8bd5d1b5f6ad8ff2f3cfa44
Tphilus <tkanateydadzie@gmail.com>
2025-10-17 17:00:23 +0000

    learn post and get post my id

--- fastapi/fastapi_learn1/main.py
+++ fastapi/fastapi_learn1/main.py
@@ -1,6 +1,8 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -9,6 +11,13 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+    
+my_post = [{"title": "itle of post 1", "content": "Content of post 1", "id": 1}, {"title": "Favorite food", "content": "I like rice", "id": 2},]
+
+def find_post(id):
+     for post in my_post:
+          if post["id"] == id: 
+               return post
 
 @app.get("/")
 def root():
@@ -16,11 +25,17 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-     return {"data": "This is your post"}
+     return {"data": my_post}
  
-@app.post("/createposts")
+@app.post("/posts")
 # def creat_posts(payload: dict = Body(...)):
 def creat_posts(new_post: Post):
-    print(new_post)
-    return {"data": "new post"}    
+     post_dict = new_post.dict()
+     post_dict["id"] = randrange(0, 100000000)
+     my_post.append(post_dict) 
+     return {"data": post_dict}
 
+@app.get("/posts/{id}")
+def get_post(id: int):
+     post = find_post(id)
+     return {"post_details": post}
